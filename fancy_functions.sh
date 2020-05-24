96b62cf60a40500fd4021ce69dae7699c6b850ad
Mr_robot <qoliver416@gmail.com>
2020-05-24 07:11:59 -0400

    functions gco

---
+++ fancy_functions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+gco ()
+{
+	if [ -d .git ]; then 
+		branch="$(git rev-parse --abbrev-ref HEAD)"
+
+		echo"[+]branch $branch"
+
+		git commit -m"[$branch] $*"
+
+		echo "[+]commit message = [$branch] $*"
+
+	else
+
+  		echo "[-]erreur!ce dossier n'est pas un dossier git";
+
+	fi;
+	
+	
+}
